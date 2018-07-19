4c3662d81f2e0d9cd39a62c947441c22f27424b6
erick.augustor <erick.augustoramos@gmail.com>
2018-07-18 22:43:26 -0300

    Data Type - String

--- basics/2-data-type.py
+++ basics/2-data-type.py
@@ -21,3 +21,16 @@ name = 'P' + last_letters
 letter = 'z'
 print(letter * 10)
 
+something = 'Hi, everything is okay with u?'
+print(something.upper())
+print(something.lower())
+print(something.split())
+print(something.split('i'))
+
+print('Hi {} everything is {}?'.format('Erick', 'okay'))
+print('Hi {1} everything is {1}?'.format('Erick', 'okay'))
+print('The {q} {b} {f}'.format(f='fox', b='brown', q='quick'))
+
+age = 3
+name = 'Sam'
+print(f'{name} is {age} years old')
